584a47407c20735c005c19d60b153eaac78587ab
orndorj19 <rendera113@gmail.com>
2017-02-26 23:53:30 -0500

    Initial upload of just file with comments

---
+++ localObjects.js
@@ -0,0 +1,5 @@
+// File: localObjects.js
+// Author: orndorj19
+// Description: A simple wrapper for localStorage to simplify storage of json objects
+// See https://github.com/orndorj19/localObjects for more information
+
